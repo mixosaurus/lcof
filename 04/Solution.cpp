a8621686edc7ca518f5ec1ddf654a34bd749803d
snake <lcpdfk@163.com>
2021-03-23 13:13:24 +0800

    deprecated

--- 04/Solution.cpp
+++ 04/Solution.cpp
@@ -7,30 +7,9 @@ bool Solution::findNumberIn2DArray(vector<vector<int>>& matrix, int target) {
         if(line.empty()){
             return false;
         }
-        if (target < line.front()) {
-            break;
-        }
-        if (containsTarget(line, target)) {
-            return true;
-        } else {
-            continue;
-        }      
-    }
-    return false;
-}
+    
 
-bool Solution::containsTarget(vector<int>& nums, int target) {
-    int low = 0;
-    int high = nums.size()-1;
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        if (target==nums[mid]) {
-            return true;
-        } else if (target > nums[mid]) {
-            low = mid + 1;
-        } else {
-            high = mid - 1;
-        }
+
     }
     return false;
 }
--- 04/Solution.h
+++ 04/Solution.h
@@ -5,6 +5,5 @@ using namespace std;
 class Solution {
 public:
     bool findNumberIn2DArray(vector<vector<int>>& matrix, int target);
-    bool containsTarget(vector<int>& nums, int target);
 };
 
