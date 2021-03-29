15ae9e297d98ea43a513bb02b17415111c7f2859
snake <lcpdfk@163.com>
2021-03-29 13:05:59 +0800

    12

--- 12/Solution.cpp
+++ 12/Solution.cpp
@@ -9,6 +9,7 @@ bool Solution::exist(vector<vector<char>>& board, string word) {
     cols = board[0].size();
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < cols; j++) {
+			//数组第一个元素与目标字符串第一个元素相同才进入递归函数
 			if (board[i][j] == word[0]) {
 				if (nextExist(board, i, j, *(new vector<coordinate>()), word,0)) {
 					return true;
@@ -19,10 +20,16 @@ bool Solution::exist(vector<vector<char>>& board, string word) {
 	return false;
 }
 
+//track保存已经过的路径坐标
 bool Solution::nextExist(vector<vector<char>>& board,int i, int j, vector<coordinate> track, string word,int k) {
-	if (i>=rows|| i < 0||j>=cols||j<0|| board[i][j] != word[k]){
+	if (i>=rows|| i < 0||j>=cols||j<0){
 		return false;
 	}
+	//当前坐标元素是否为目标字符串中的相应元素
+	if (board[i][j] != word[k]){
+		return false;
+	}
+	//检查当前坐标是否存在于已经过路径中
 	coordinate coord = { i,j };
 	if(find(track.begin(), track.end(),coord)!=track.end()){
 		return false;
@@ -30,6 +37,7 @@ bool Solution::nextExist(vector<vector<char>>& board,int i, int j, vector<coordi
 	if (k == word.size() - 1) {
 		return true;
 	}
+	//把当前坐标加入路径中
 	coordinate coord2 = { i, j };
 	track.push_back(coord2);
 	return  nextExist(board, i - 1, j, track, word,k+1)||
